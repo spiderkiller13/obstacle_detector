2786a3a336db8b5b49ba77c0837e92bd31f21178
lucky Yu <luckyyu910645@gmail.com>
2020-08-30 13:36:52 +0800

    add car2 tf

--- script/laser_find_shelf.py
+++ script/laser_find_shelf.py
@@ -360,6 +360,8 @@ class Two_shelf_finder():
         # Send tf
         if ROLE == "leader":
             send_tf((TOW_CAR_LENGTH/2.0, 0, theta), "carB/base_link", "car1/base_link")
+        elif ROLE == "follower":
+            send_tf((-TOW_CAR_LENGTH/2.0, 0, theta), "carB/base_link", "car2/base_link")
 
 if __name__ == '__main__':
     rospy.init_node('laser_find_shelf',anonymous=False)
